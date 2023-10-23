8f98c7b659bb2a2dfee4524e6aafd97f354682c2
czal1011 <czal1011@h-ka.de>
2023-10-23 13:25:59 +0000

    [UPDATE] Added logs and finished Task 1

--- lab1/clientserver.py
+++ lab1/clientserver.py
@@ -46,12 +46,19 @@ class Server:
             try:
                 # pylint: disable=unused-variable
                 (connection, address) = self.sock.accept()  # returns new socket and address of client
+                self._logger.info("Accepted client connection")
                 while True:  # forever
                     data = connection.recv(1024)  # receive data from client
                     if not data:
                         break  # stop if client stopped
+                    self._logger.info("Received data from client. Analyzing...")
                     data_decoded = data.decode('ascii')
-                    if data_decoded == 'get_all':
+                    if data_decoded == 'CLOSE':
+                        self._logger.info("Executing method 'CLOSE', terminating connection...")
+                        self._serving = False
+                        break
+                    if data_decoded == 'GETALL':
+                        self._logger.info("Executing method 'GETALL'...")
                         if len(self._phone_database) == 0:
                             connection.send("Warning: Phone number database is empty.".encode('ascii'))
                         else:
@@ -61,73 +68,87 @@ class Server:
                                 number = entry[1]
                                 name_number_pair_string = f"{name}: {number}|"
                                 return_string += name_number_pair_string
+                            self._logger.info(f"Successfully found phone number for {len(self._phone_database)} names.")
                             connection.send(return_string[:-1].encode('ascii'))  # send return string, minus the last '|' character
-                    elif data_decoded.startswith("get/"):
-                        if data_decoded == 'get/':
+                    elif data_decoded.startswith("GET/"):
+                        self._logger.info(f"Executing method 'GET' for name {data_decoded[4:]}...")
+                        if data_decoded == 'GET/':
                             connection.send("Warning: Invalid parameter '' for method 'GET'.".encode('ascii'))
                             break
                         name = data_decoded[4:]
                         if name in self._phone_database:
+                            self._logger.info(f"Name {name} found, returning phone number...")
                             # return string format: "name: number"
                             number = self._phone_database[name]
                             return_string = f"{name}: {number}"
+                            self._logger.info(f"Successfully found phone number {number} for {name}.")
                             connection.send(return_string.encode('ascii'))
                         else:
                             connection.send(f"Warning: Name {name} is not present in the phone number database.".encode('ascii'))
                     else:
                         self._logger.warning(f"Warning: Invalid method '{data_decoded}'.")
                         connection.send(f"Warning: Invalid method '{data_decoded}'.".encode('ascii'))
+                self._logger.info("Closing connection...")
                 connection.close()  # close the connection
             except socket.timeout:
                 pass  # ignore timeouts
+        self._logger.info("Closing socket...")
         self.sock.close()
         self._logger.info("Server down.")
 
 
 class Client:
     """ The client """
-    logger = logging.getLogger("vs2lab.a1_layers.clientserver.Client")
+    _logger = logging.getLogger("vs2lab.a1_layers.clientserver.Client")
 
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect((const_cs.HOST, const_cs.PORT))
-        self.logger.info("Client connected to socket " + str(self.sock))
+        self._logger.info("Client connected to socket " + str(self.sock))
 
     def call(self, msg_in="Hello, world"):
         """ Call server """
+        self._logger.info(f"Sending data {msg_in} to server...")
         self.sock.send(msg_in.encode('ascii'))  # send encoded string as data
         data = self.sock.recv(1024)  # receive the response
+        self._logger.info("Obtained data from server, analyzing...")
         msg_out = data.decode('ascii')
-        # print the result
+        self._logger.info("Printing decoded data to console...")
         self.print_message(msg_out)
+        self._logger.info("Successfully printed decoded data. Closing socket connection...")
         self.sock.close()  # close the connection
-        self.logger.info("Client down.")
+        self._logger.info("Client down.")
         return msg_out
 
     def print_message(self, _message):
         """Prints the message received by the server."""
-        self.logger.info("=================================================")
+        self._logger.info("=================================================")
         if _message.startswith('GETALL|'):  # GETALL method return string format: "GETALL|name: number|name: number|..."
             name_number_pairs = _message[7:].split('|') # remove "GETALL|"
             for entry in name_number_pairs:
-                self.logger.info(entry)
+                self._logger.info(entry)
         else:
             if _message.startswith("Warning"):
-                self.logger.warning(_message)  # warnings
+                self._logger.warning(_message)  # warnings
             else:
-                self.logger.info(_message)  # GET method
-        self.logger.info("=================================================\n")
+                self._logger.info(_message)  # GET method
+        self._logger.info("=================================================\n")
 
 
     def get(self, _name):
         """Attempts to call the server to get a phone number based on the parameter name."""
-        self.logger.info(f"Attempting to use method \"GET/{_name}\"...")
-        self.call(msg_in=f"get/{_name}")
+        self._logger.info(f"Attempting to use method \"GET/{_name}\"...")
+        return self.call(msg_in=f"GET/{_name}")
 
     def get_all(self):
         """Attempts to call the server to get all existing phone numbers."""
-        self.logger.info("Attempting to use method \"GETALL\"...")
-        self.call(msg_in="get_all")
+        self._logger.info("Attempting to use method \"GETALL\"...")
+        return self.call(msg_in="GETALL")
+
+    def close_connection(self):
+        """Tell server to close the connection server-side."""
+        self._logger.info("Telling server to close connection...")
+        self.call(msg_in="CLOSE")
 
     def close(self):
         """ Close socket """
--- lab1/test_clientserver.py
+++ lab1/test_clientserver.py
@@ -1,3 +1,4 @@
+# pylint: disable=protected-access
 """
 Simple client server unit test
 """
@@ -11,6 +12,7 @@ from context import lab_logging
 
 lab_logging.setup(stream_level=logging.INFO)
 
+unittest.TestLoader.sortTestMethodsUsing = None
 
 class TestEchoService(unittest.TestCase):
     """The test"""
@@ -25,11 +27,54 @@ class TestEchoService(unittest.TestCase):
         super().setUp()
         self.client = clientserver.Client()  # create new client for each test
 
+    x = """
     # TODO write more tests & modify this one below
     def test_srv_get(self):  # each test_* function is a test
-        """Test simple call"""
+        \"""Test simple call\"""
         msg = self.client.call("Hello VS2Lab")
         self.assertEqual(msg, 'Hello VS2Lab*')
+    """
+
+    def test_1_get_rufus_should_return_name_and_number_string(self):
+        """Test the GET method with a name that already exists in the phone number database
+        (as defined by the constructor)."""
+        msg = self.client.get("Rufus")
+        self.assertEqual(msg, 'Rufus: 1234567890')
+
+    def test_2_get_nonexistent_name_should_return_name_not_present_warning(self):
+        """Test the GET method with a name that doesn't exist in the phone number database."""
+        msg = self.client.get("Carlos")
+        self.assertEqual(msg, 'Warning: Name Carlos is not present in the phone number database.')
+
+    def test_3_get_all_should_return_long_string_with_all_entries_as_name_and_number_duo(self):
+        """Test the GETALL method."""
+        msg = self.client.get_all()
+        self.assertEqual(msg, 'GETALL|Rufus: 1234567890|Karl-Heinz-Dietrich: 987654321|Peter: 89555521|Peter 2: 2222222223|Sonic the Hedgehog: 9876567811')
+
+    def test_4_add_phone_number_should_add_it_to_the_database_and_enlarge_it(self):
+        """Test the "add_phone_number" server method."""
+        self._server.add_phone_number("Martin", 5064234)
+        msg = self.client.get("Martin")
+        self.assertEqual(msg, "Martin: 5064234")
+        self.assertEqual(len(self._server._phone_database), 6)
+
+    def test_5_get_empty_string_should_return_invalid_parameter_for_get_string(self):
+        """Test the GET method with an empty string as parameter,
+        which should return a warning string."""
+        msg = self.client.get("")
+        self.assertEqual(msg, "Warning: Invalid parameter '' for method 'GET'.")
+
+    def test_6_method_call_with_invalid_parameter_should_return_invaild_method_warning(self):
+        """Test the call client method with a custom message parameter,
+        which should go against the client-server-protocol and return a warning."""
+        msg = self.client.call("gaming")
+        self.assertEqual(msg, "Warning: Invalid method 'gaming'.")
+
+    def test_7_method_call_with_get_parameter_should_return_name_and_number_string(self):
+        """Test the call client method with a valid message parameter,
+        which should work exactly like calling the method itself."""
+        msg = self.client.call("GET/Rufus")
+        self.assertEqual(msg, 'Rufus: 1234567890')
 
     def tearDown(self):
         self.client.close()  # terminate client after each test
