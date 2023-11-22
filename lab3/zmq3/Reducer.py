443895272d2b2c779ae6f055fe3027795a1c0893
czal1011 <czal1011@h-ka.de>
2023-11-22 11:33:06 +0000

    Decoded Reducer work & Added a very looong example

--- lab3/zmq3/Reducer.py
+++ lab3/zmq3/Reducer.py
@@ -36,7 +36,7 @@ print("{} started".format(me))
 while True:
     work = sub_socket.recv()
     print(f"Reducer {me} received workload.")
-    word = work[16:]
+    word = work.decode("utf-8")[18:]
     if(word in counted_words):
         counted_words[word] = counted_words[word] + 1
     else:
@@ -44,5 +44,5 @@ while True:
     print(f"--------------------------\n"
     + f"Reducer {me} added word {word}. Aggregated words:\n")
     for counted_word in counted_words:
-        print(f"{str(counted_word)[4:-1]}: {counted_words[counted_word]}")
+        print(f"{str(counted_word)}: {counted_words[counted_word]}")
     print(f"--------------------------\n")
